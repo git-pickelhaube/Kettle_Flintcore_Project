04ddb3ad5168ce95c56fff07dcb423015bfce771
git-pickelhaube <isabella.totally.rocks@protonmail.com>
2026-07-14 19:06:10 -0400

    Added items with no functionality.
    Includes textures.
    
    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-"# Kettle_Flintcore_Project" 
+# Kettle Flintcore Project (working title)
+**WIP, may or may not be finished ever**
+
+Weapon / gun mod made using Flintcore.
+
+Flintcore can be found here: https://github.com/Livelandr/flintcore/tree/main.
+
+Licensed under the *LGPL-3.0 license*. 
+
+May contain fragments of code from https://github.com/Raging-Zombies/flintnpowder.
+
+**This project is NOT a fork of Flint'N'Powder.**
+
+Thank you to Kaupenjoe for his modding tutorials. 
+
+You can find his channel here: https://www.youtube.com/channel/UCbzPhyLcO8VP25dZ7kaUyAw
--- src/main/java/net/kettle/kettlehelmetsmod/KettleMod.java
+++ src/main/java/net/kettle/kettlehelmetsmod/KettleMod.java
@@ -1,6 +1,9 @@
 package net.kettle.kettlehelmetsmod;
 
 import com.mojang.logging.LogUtils;
+import net.kettle.kettlehelmetsmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +30,8 @@ public class KettleMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -45,7 +50,11 @@ public class KettleMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.COMBAT) { //only if the tab is "Combat"
+            event.accept(ModItems.GUN); //add our basic item ("GUN")
+            //add bullets
+            event.accept(ModItems.BULLET);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
